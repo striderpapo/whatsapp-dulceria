5c0ab408e145c2ff60af357a3650ec4db8e9ed94
striderpapo <78055747+striderpapo@users.noreply.github.com>
2026-08-31 14:15:08 -0600

    Recibir webhook de whats

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 
 const app = express();
 
@@ -37,4 +37,27 @@ app.post("/webhook", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
+});*/
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("🍬 Bot de WhatsApp funcionando");
+});
+
+app.post("/webhook", (req, res) => {
+    console.log("📩 Webhook recibido");
+
+    console.log(JSON.stringify(req.body, null, 2));
+
+    res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor escuchando en el puerto ${PORT}`);
 });
